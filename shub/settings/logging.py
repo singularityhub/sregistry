595a81445ae6ccb2c61e1c1409ddebde018c7533
David Trudgian <david.trudgian@utsouthwestern.edu>
2018-02-02 10:10:13 -0600

    Move logging config to separate file - missed file

---
+++ shub/settings/logging.py
@@ -0,0 +1,17 @@
+# Default Django logging is WARNINGS+ to console
+# so visible via docker-compose logs uwsgi
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'WARNING'),
+        },
+    },
+}
