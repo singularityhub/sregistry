83a2dd0f8ea3c193330fb5d47fd24c9ede689f7a
Vanessa Sochat <vsochat@stanford.edu>
2018-04-20 01:53:05 -0400

    updating config

--- shub/logger.py
+++ shub/logger.py
@@ -1,8 +1,8 @@
 '''
 
+Copyright (C) 2017-2018 Vanessa Sochat.
 Copyright (C) 2017-2018 The Board of Trustees of the Leland Stanford Junior
 University.
-Copyright (C) 2017-2018 Vanessa Sochat.
 
 This program is free software: you can redistribute it and/or modify it
 under the terms of the GNU Affero General Public License as published by
