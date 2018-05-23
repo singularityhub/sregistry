95160c7919eb2bf3b12ec15bb19f9f98580ba400
Vanessa Sochat <vsochat@stanford.edu>
2018-05-23 07:26:11 -0700

    we dont even need this function

--- shub/apps/logs/utils.py
+++ shub/apps/logs/utils.py
@@ -21,7 +21,6 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
 from shub.apps.main.models import Container
-from sregistry.utils import read_json
 
 from itertools import chain
 import os
