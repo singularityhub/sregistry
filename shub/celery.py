a9eaed25f5bc3b3889ed9755e29a7d9e2437a9a8
Vanessa Sochat <vsochat@stanford.edu>
2019-06-07 15:48:43 -0400

    adding app names back in
    Signed-off-by: Vanessa Sochat <vsochat@stanford.edu>

--- shub/celery.py
+++ shub/celery.py
@@ -11,6 +11,7 @@ with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 from __future__ import absolute_import, unicode_literals
 import os
 from django.conf import settings
+from django.apps import apps
 from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shub.settings')
@@ -20,5 +21,7 @@ shubcelery = Celery('shub')
 # pickle the object when using Windows.
 shubcelery.config_from_object('django.conf:settings', namespace="CELERY")
 shubcelery.conf.imports = settings.CELERY_IMPORTS
-shubcelery.autodiscover_tasks()
- 
+
+# This is important! autodiscover_tasks() is supposed to work without
+# providing names, but it doesn't.
+shubcelery.autodiscover_tasks(lambda: [a.name for a in apps.get_app_configs()])
