73233ea90ae9e88a8594512c030a28144c8ccf1d
Vanessa Sochat <vsochat@stanford.edu>
2019-06-07 13:32:51 -0400

    forgot to add to_space
    Signed-off-by: Vanessa Sochat <vsochat@stanford.edu>

---
+++ shub/apps/singularity/templatetags/to_space.py
@@ -0,0 +1,19 @@
+'''
+
+Copyright (C) 2019 Vanessa Sochat.
+
+This Source Code Form is subject to the terms of the
+Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed
+with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+'''
+
+from django import template
+
+register = template.Library()
+
+@register.filter
+def to_space(value):
+    if isinstance(value, str):
+        return value.replace("_"," ")
+    return value
