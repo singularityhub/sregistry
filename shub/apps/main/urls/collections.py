90eb9455927c7d3e54b55c8050cf7d4f081442d9
Vanessa Sochat <vsochat@stanford.edu>
2019-06-10 14:45:50 -0400

    killed usage page
    Signed-off-by: Vanessa Sochat <vsochat@stanford.edu>

--- shub/apps/main/urls/collections.py
+++ shub/apps/main/urls/collections.py
@@ -19,13 +19,13 @@ urlpatterns = [
     url(r'^collections/new/?$', views.new_collection,name='new_collection'),
     url(r'^collections/(?P<cid>\d+)/settings/?$', views.collection_settings,name='collection_settings'),
     url(r'^collections/(?P<cid>\d+)/contributors/?$', views.edit_contributors,name='edit_contributors'),
-    url(r'^collections/(?P<username>.+?)/(?P<reponame>.+?)/?$', views.view_named_collection, name='collection_byname'),
     url(r'^collections/(?P<cid>\d+)/?$', views.view_collection, name='collection_details'),
     url(r'^collections/my/?$', views.my_collections,name='my_collections'),
     url(r'^collections/(?P<cid>\d+)/usage/?$', views.collection_commands,name='collection_commands'),
     url(r'^collections/(?P<cid>\d+)/delete/?$', views.delete_collection,name='delete_collection'),
     url(r'^collections/(?P<cid>\d+)/private/?$',views.make_collection_private,name='make_collection_private'),
-    url(r'^collections/(?P<cid>\d+)/public/?$', views.make_collection_public,name='make_collection_public')
+    url(r'^collections/(?P<cid>\d+)/public/?$', views.make_collection_public,name='make_collection_public'),
+    url(r'^collections/(?P<username>.+?)/(?P<reponame>.+?)/?$', views.view_named_collection, name='collection_byname')
 
 ]
 
