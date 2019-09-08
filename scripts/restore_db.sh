35d3552b5c96ea284cd352effafe07e2c5e9258c
Vanessa Sochat <vsochat@stanford.edu>
2019-09-08 16:44:00 -0400

    adding scripts (and cron) to backup on nightly basis and restore (manual)
    Signed-off-by: Vanessa Sochat <vsochat@stanford.edu>

---
+++ scripts/restore_db.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# **Important** run this command from inside the container, where you have
+# Django and dependencies installed. This is typically run automatically 
+# (nightly) via a cron job. See the Dockerfile, or shell into the uwsgi container:
+# > service cron status
+# > crontab -l
+# > crontab -e
+
+for db in "users" "api" "main" "logs" 
+  do
+    echo "Loading table $db"
+    python /code/manage.py loaddata /code/backup/$db.json
+done
