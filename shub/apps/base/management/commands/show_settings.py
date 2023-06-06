6d8ad6f2e3a000abc51b4351185e65e0afee4c02
Evan J. Felix <evan.felix@pnnl.gov>
2023-06-05 17:32:27 -0700

    remove header as the MPL does not require them in all files

--- shub/apps/base/management/commands/show_settings.py
+++ shub/apps/base/management/commands/show_settings.py
@@ -1,13 +1,3 @@
-"""
-
-Copyright 2017-2023 Evan Felix.
-
-This Source Code Form is subject to the terms of the
-Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed
-with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
-"""
-
 import shub.settings as cfg
 from django.core.management.base import BaseCommand
 
